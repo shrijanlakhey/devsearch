9bdb119a3ace24c27c1dddd41f72dad43acee8e1
shrijanlakhey <lakheyshrijan@gmail.com>
2023-03-25 21:28:25 +0545

    added simple templates

--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def projects(request):
-    return HttpResponse('Here are your products')
+    return render(request, 'projects.html')
 
 def project(request, pk):
-    return HttpResponse('Single Project' + ' ' + str(pk) )
+    return render(request, 'single-project.html')
