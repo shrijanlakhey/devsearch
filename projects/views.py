360a3e40920c052dc09e817ab1156c7ef164d7b1
shrijanlakhey <lakheyshrijan@gmail.com>
2023-04-01 23:15:18 +0545

    added new folder projects inside templates folder

--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def projects(request):
-    return render(request, 'projects.html')
+    return render(request, 'projects/projects.html')
 
 def project(request, pk):
-    return render(request, 'single-project.html')
+    return render(request, 'projects/single-project.html')
