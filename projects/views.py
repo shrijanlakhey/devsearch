bdcbb4c3a98d133623d682985eaecb96e6146128
shrijanlakhey <lakheyshrijan@gmail.com>
2023-04-08 19:11:03 +0545

    tried python dictionary with html tags

--- projects/views.py
+++ projects/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def projects(request):
-    msg = 'Hello, you are on the projects page'
-    return render(request, 'projects/projects.html', {'message':msg})
+    page = 'projects'
+    return render(request, 'projects/projects.html', {'page':page})
     # message is the key and msg is the value
 
 def project(request, pk):
