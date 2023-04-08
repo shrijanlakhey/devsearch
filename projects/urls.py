e3b57ad12c16a11c99009a48933dc1ac6ef9fb09
shrijanlakhey <lakheyshrijan@gmail.com>
2023-04-08 19:08:06 +0545

    tried python dictionary

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('projects/',views.projects, name="projects"),
+    path('',views.projects, name="projects"),  #http://127.0.0.1:8000/(root doamin) mah jada projects.html load hunxa
     path('project/<str:pk>/', views.project, name="project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    msg = 'Hello, you are on the projects page'
+    return render(request, 'projects/projects.html', {'message':msg})
+    # message is the key and msg is the value
 
 def project(request, pk):
     return render(request, 'projects/single-project.html')
