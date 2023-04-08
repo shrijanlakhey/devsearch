e4fcd9f7faeb7803f70108e09c22ba6a9c417141
shrijanlakhey <lakheyshrijan@gmail.com>
2023-04-08 19:19:15 +0545

    tried out if statement

--- projects/views.py
+++ projects/views.py
@@ -4,7 +4,12 @@ from django.http import HttpResponse
 
 def projects(request):
     page = 'projects'
-    return render(request, 'projects/projects.html', {'page':page})
+    number = 10
+    context = {
+        'page': page, 
+        'number': number,
+    }
+    return render(request, 'projects/projects.html', context)
     # message is the key and msg is the value
 
 def project(request, pk):
