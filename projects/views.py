b69fc8ec40cfb19af4079c4f03b763bea2bacb29
shrijanlakhey <lakheyshrijan@gmail.com>
2023-04-08 19:37:06 +0545

    tried out python dictionary and list

--- projects/views.py
+++ projects/views.py
@@ -2,12 +2,36 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+projectList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'This was a project where I built out my portfolio'
+    },
+    
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'Awesome open source project I am still working on it'
+    },
+
+
+]
+
+# we access a dictonary's value in template by its key
 def projects(request):
     page = 'projects'
     number = 10
     context = {
         'page': page, 
         'number': number,
+        'projects': projectList,
     }
     return render(request, 'projects/projects.html', context)
     # message is the key and msg is the value
