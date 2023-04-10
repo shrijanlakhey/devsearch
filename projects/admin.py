8ba62240fd056ad3a950100a7b615e5ecfff8e60
shrijanlakhey <lakheyshrijan@gmail.com>
2023-04-10 21:52:48 +0545

    created models named Review  nd tag it to admin and created a many to many relationship between Project and Tag models

--- projects/admin.py
+++ projects/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Project # importing model
+from .models import Project, Review, Tag # importing model
 # even after we created model in models.py file, it will not show in django admin
 # we need to register it first
 admin.site.register(Project) # registering the model
+admin.site.register(Review)
+admin.site.register(Tag) 
--- projects/models.py
+++ projects/models.py
@@ -7,10 +7,43 @@ class Project(models.Model):
     description = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True) # you do not need to add the Tag in quote if the Tag model is above the Project model but it is not above the project model in this case so we need to add it within a quote
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True) # gives date and a time stamp
     # auto_now_add=True generates a timestamp whenever a new project entry/model instance is created
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False) # uuid4 is an encoding type, unique=True means no other value in the database can have the  same number
     # no need to create an id field whenever we are inheriting from 'models.Model' because django creates it by itself, by default it is of int type
     # but it is recommened to use uuid 
-# null=True, blank=True means that the user is able to submit the form even if the field (eg, description) is empty or blank 
-# in django, by default null value is False which means the fields should not be left empty
+    # null=True, blank=True means that the user is able to submit the form even if the field (eg, description) is empty or blank 
+    # in django, by default null value is False which means the fields should not be left empty
+
+
+    # anytime we access the model and not it's attribute, it will be output with the title value
+    def __str__(self):
+        return self.title
+    
+class Review(models.Model):
+    # VOTE_TYPE is a tuple
+    VOTE_TYPE = (
+        ('up', 'Up Vote'), # we can reference this vote by 'up' in db but 'Up Vote' is what it's gonna be displayed as to the users 
+        ('down', 'Down Vote'),
+    )
+    
+    # owner = 
+    project = models.ForeignKey(Project, on_delete=models.CASCADE) # [on_delete=models.CASCADE] will delete all the reviews if the project is deleted
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True) 
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False) 
+
+    def __str__(self):
+        return self.value
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True) 
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False) 
+
+    def __str__(self):
+        return self.name
