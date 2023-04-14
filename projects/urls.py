f5b7aa8c981ca7d2a1ebd52a9ab582915a37bbf5
shrijanlakhey <lakheyshrijan@gmail.com>
2023-04-14 10:39:10 +0545

    added a new form to add projects

--- projects/urls.py
+++ projects/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('',views.projects, name="projects"),  #http://127.0.0.1:8000/(root doamin) mah jada projects.html load hunxa
     path('project/<str:pk>/', views.project, name="project"),
+    path('create-project/', views.createProject, name="create-project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -3,26 +3,6 @@ from django.http import HttpResponse
 from .models import Project
 # Create your views here.
 
-projectList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website'
-    },
-    
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'This was a project where I built out my portfolio'
-    },
-    
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'Awesome open source project I am still working on it'
-    },
-]
-
 # we access a dictonary's value in template by its key
 def projects(request):
     projects = Project.objects.all()
@@ -35,3 +15,7 @@ def projects(request):
 def project(request, pk):
     projectObj = Project.objects.get(id = pk)
     return render(request, 'projects/single-project.html', {'project': projectObj})
+
+def createProject(request):
+    context = {}
+    return render(request, 'projects/project_form.html')
