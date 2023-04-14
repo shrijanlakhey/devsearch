8106b036aebac515600d4dc95deb727ce2f5b0e0
shrijanlakhey <lakheyshrijan@gmail.com>
2023-04-14 16:38:48 +0545

    added only required fields in form and added feature of adding new entries to database through project_form

--- projects/forms.py
+++ projects/forms.py
@@ -4,9 +4,6 @@ from .models import Project
 class ProjectForm(ModelForm):
     class Meta:
         # django's gonna look at this model(i.e Project) and it's gonna create a form based on this model
-        # it's gonna look at all of the attributes, see the type of fields and generate a from
-        # since  "fields = '__all__' ", it's going to generate a field for attribute of the model
-        # but id won't be there because it is not editable
-        # and created won't be there because it is automatically generated
+        # it's gonna look at the specified attributes, see the type of fields and generate a from
         model = Project 
-        fields = '__all__'
+        fields = ['title', 'description', 'demo_link', 'source_link', 'tags']
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project
 from .forms import ProjectForm
@@ -19,5 +19,13 @@ def project(request, pk):
 
 def createProject(request):
     form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
     context = {'form': form,}
     return render(request, 'projects/project_form.html', context)
+
+
+
