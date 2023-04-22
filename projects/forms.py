1b592e5faae1292365358ee32d8839b31a9a2049
shrijanlakhey <lakheyshrijan@gmail.com>
2023-04-22 11:23:12 +0545

    added better style and jinja template to project form page

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 class ProjectForm(ModelForm):
@@ -7,3 +8,17 @@ class ProjectForm(ModelForm):
         # it's gonna look at the specified attributes, see the type of fields and generate a from
         model = Project 
         fields = ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tags']
+
+        # modifying the widgets
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(), 
+        }
+    
+    # overwriting __init__ method
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        # self.fields['title'].widget.attrs.update({'class':'input'}) # updatiing class attributes 'class' is set to 'input' because I want to get the css for the input class
+
+        for name, field in self.fields.items(): # name = key, field = value
+            field.widget.attrs.update({'class':'input'})
