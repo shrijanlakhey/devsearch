1890a1785982e68c2ed9d03bf8fd721b7beaa745
shrijanlakhey <lakheyshrijan@gmail.com>
2023-04-28 10:33:36 +0545

    created Profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import uuid
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True) # user will have one to one rleationship with User model(dajngo's built in model), 
+    # every single user in the db will have a profile and every profile will ahve a user and profiel will get automaticlly generated everytime a user is created
+    # 'on_delete=models.CASCADE', everytime user is deleted, hs porfile is also deleted
+    name = models.CharField(max_length=200, blank=True, null=True)
+    email = models.EmailField(max_length=500, blank=True, null=True)
+    short_intro = models.CharField(max_length=500, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    profile_image = models.ImageField(blank=True, null=True, upload_to='profiles/', default='profiles/user-default.png')
+    
+    social_github = models.CharField(max_length=200, blank=True, null=True)
+    social_twitter = models.CharField(max_length=200, blank=True, null=True)
+    social_linkedin = models.CharField(max_length=200, blank=True, null=True)
+    social_youtube = models.CharField(max_length=200, blank=True, null=True)
+    social_website = models.CharField(max_length=200, blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    
+    def __str__(self):
+        return str(self.user.username) # if we want to use a number here then we need to wrap that in a string method (str)
