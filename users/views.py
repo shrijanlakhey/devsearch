ed30f478dd2cbc38c0b38de9acaa26715da2f253
shrijanlakhey <lakheyshrijan@gmail.com>
2023-04-29 19:12:33 +0545

    added userProfile and since the icons were not loading used text for github,etc. link as temp solution

--- users/views.py
+++ users/views.py
@@ -7,3 +7,8 @@ def profiles(request):
     profiles = Profile.objects.all() # getting all the profiles from the db
     context = {'profiles':profiles}
     return render(request, 'users/profiles.html', context)
+
+def userProfile(request,pk):
+    profile = Profile.objects.get(id=pk) # get method gets back a single object
+    context = {'profile': profile}
+    return render(request, 'users/user-profile.html', context)
