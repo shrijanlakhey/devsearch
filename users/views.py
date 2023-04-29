99614e3a56392bcd6193588684adce1b6f53c218
shrijanlakhey <lakheyshrijan@gmail.com>
2023-04-29 19:27:00 +0545

    cahnged skills in userprofile page, if skill has description, it will be displayed with it else it will be displayed in other skills

--- users/views.py
+++ users/views.py
@@ -4,11 +4,20 @@ from . models import Profile
 
 
 def profiles(request):
-    profiles = Profile.objects.all() # getting all the profiles from the db
-    context = {'profiles':profiles}
+    profiles = Profile.objects.all()  # getting all the profiles from the db
+    context = {'profiles': profiles}
     return render(request, 'users/profiles.html', context)
 
-def userProfile(request,pk):
-    profile = Profile.objects.get(id=pk) # get method gets back a single object
-    context = {'profile': profile}
+
+def userProfile(request, pk):
+    # get method gets back a single object
+    profile = Profile.objects.get(id=pk)
+    # if skill does not have any description or is blank, it will be excluded
+    topSkills = profile.skill_set.exclude(description__exact="")
+    # if skill has an empty string or blank as a description, it will be selected
+    othrtSkills = profile.skill_set.filter(description="")
+    context = {'profile': profile,
+               'topSkills': topSkills,
+               'otherSkills': othrtSkills,
+               }
     return render(request, 'users/user-profile.html', context)
