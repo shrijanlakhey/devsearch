f43e95acd66ff41f68b186d532ea652876646881
shrijanlakhey <lakheyshrijan@gmail.com>
2023-05-01 10:29:23 +0545

    now only logged in users can see the add project in nav bar and create, update or delete project only if they are logged in

--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project
 from .forms import ProjectForm
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 # we access a dictonary's value in template by its key
@@ -21,33 +23,38 @@ def project(request, pk):
     return render(request, 'projects/single-project.html', {'project': projectObj})
 
 
+# a decorator that lets user access this age only if they are logged in and send them to the login page if they are not logged in
+@login_required(login_url="login")
 def createProject(request):
     form = ProjectForm()
     if request.method == 'POST':
-        form = ProjectForm(request.POST, request.FILES) # request.FILES is getting those files/images sent
+        # request.FILES is getting those files/images sent
+        form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('projects')
     context = {'form': form, }
     return render(request, 'projects/project_form.html', context)
 
-
+@login_required(login_url="login")
 def updateProject(request, pk):
     project = Project.objects.get(id=pk)
     # instead of rendering a form, we are gonna take an instance, it is gonna pre-fill all the form fields of that projects data
     form = ProjectForm(instance=project)
     if request.method == 'POST':
-        form = ProjectForm(request.POST, request.FILES, instance=project) # letting django know which project is being updated
+        # letting django know which project is being updated
+        form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid():
             form.save()
             return redirect('projects')
     context = {'form': form, }
     return render(request, 'projects/project_form.html', context)
 
+@login_required(login_url="login")
 def deleteProject(request, pk):
     project = Project.objects.get(id=pk)
     if request.method == 'POST':
         project.delete()
         return redirect('projects')
-    context = {'object':project }
+    context = {'object': project}
     return render(request, 'projects/delete_template.html', context)
